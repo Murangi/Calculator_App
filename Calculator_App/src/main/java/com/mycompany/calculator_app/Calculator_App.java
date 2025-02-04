b13f9f2bac14f8b89809eecff8896f9436c9c4fd
Murangi <nemandalalimurangi7@gmail.com>
2025-02-04 20:42:49 +0200

    inital push

---
+++ Calculator_App/src/main/java/com/mycompany/calculator_app/Calculator_App.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator_app;
+
+/**
+ *
+ * @author User
+ */
+import javax.swing.*;
+import java.awt.*;
+impot java.awt.event.*;
+
+public class Calculator_App implements ActionListener
+{
+    
+    Calculator_App()
+    {
+        
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
